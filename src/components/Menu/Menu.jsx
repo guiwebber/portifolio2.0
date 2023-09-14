f0287e9dfed4fc6d9194f975f7eb91cf20029085
Guilherme webber <guiz1n.webber@gmail.com>
2023-09-14 15:40:20 -0300

    menu mobile and other things

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import "./Menu.css";
+import gwLogo from "../../assets/img/gw.png";
 const Menu = () => {
   return (
     <nav className="menuTop">
-      <a href="">Projetos</a>
-      <a href="">Sobre mim</a>
-      <a href="">Contatos</a>
-      <a href="">Habilidades</a>
-      <a href="">Certificações</a>
+      <div className="logoDesk">
+        <img src={gwLogo} alt="" />
+      </div>
+      <div className="maxWidth flexTop">
+        <div className="divLinks">
+          <a className="links line" href="">Projetos</a>
+          <a className="links line" href="">Sobre mim</a>
+          <a className="links line" href="">Contatos</a>
+          <a className="links line" href="">Habilidades</a>
+          <a className="links line" href="">Certificações</a>
+        </div>
+      </div>
+      <div className="menuMob">
+      <div className="logoMob">
+        <img src={gwLogo} alt="" />
+      </div>
+        <input className="checkBoxInput" type="checkbox" />
+        <span></span>
+        <div className="menuMobile">
+          <a className="links line" href="">Projetos</a>
+          <a className="links line" href="">Sobre mim</a>
+          <a className="links line" href="">Contatos</a>
+          <a className="links line" href="">Habilidades</a>
+          <a className="links line" href="">Certificações</a>
+        </div>
+      </div>
     </nav>
   );
 };
